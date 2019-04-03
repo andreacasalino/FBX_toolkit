93d1484195017bc0f6462efeef0aeb3ee6ba9c42
Andrea <47759758+andreacasalino@users.noreply.github.com>
2019-04-04 01:03:05 +0200

    Add files via upload

---
+++ Usage/T2_Inverse_Kinematics/Example_of_Inversion.bat
@@ -0,0 +1,3 @@
+"../../x64/Release/T2_Inverse_Kinematics.exe" "Target_to_invert.txt" "Target_inverted.txt"
+REM use flag -R for printing also the residual (poses end effectors - target)
+"../../x64/Release/T2_Inverse_Kinematics.exe" "Target_to_invert.txt" "Target_inverted.txt" "-R:"
